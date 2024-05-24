c3b2172b3325655a036b4bf08303c516969aa526
asapluiz <asimhi.loui@gmail.com>
2024-05-24 12:14:42 +0200

    make all refactor to info endpoint

--- lambda_functions/modules/infos/info_service.ts
+++ lambda_functions/modules/infos/info_service.ts
@@ -31,7 +31,6 @@ export class InfoService{
               sk: 'info'
             }
         }
-    
         const data = await this.dynamoDb.get(params).promise() 
 
         if(!data.Item){
--- lambda_functions/routes/infos/index.ts
+++ lambda_functions/routes/infos/index.ts
@@ -1,32 +1,20 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Handler, Context } from 'aws-lambda';
 import { errorWrapper } from '../../error_handler/error_wrapper';
-import SchemaValidator from '../../validation/validator';
 import { InfoController } from '../../modules/infos/info_controller';
 import { InfoService } from '../../modules/infos/info_service';
 import { ApiError } from '../../error_handler/error_handler';
-import infoDataSchema from '../../modules/infos/schemas/info_schema';
-import { InfoData } from '../../modules/infos/info_types';
-import { dummy_info_data } from '../../test/dummy_data';
-
 
 
 const handler: Handler = async (event: APIGatewayProxyEvent, context:Context): Promise<APIGatewayProxyResult> => {
-
-    
-    // const simulation_validator = new SchemaValidator(infoDataSchema);
-    // const info_service = new InfoService
-    // const info_controller = new InfoController(info_service)
-
-    // return await info_controller.updateInfo<InfoData>( dummy_info_data, simulation_validator)
-    // ..............................................................................
     const method = event.httpMethod;
     const info_service = new InfoService
 
     if (method === 'POST') { 
-        const simulation_validator = new SchemaValidator(infoDataSchema);
+        if(!event.body){
+            throw new ApiError({message:'request must have a body', code:400, status:'Bad request'})
+        }
         const info_controller = new InfoController(info_service)
-
-        return await info_controller.updateInfo<InfoData>( JSON.parse(event.body?? ""), simulation_validator)
+        return await info_controller.updateInfo(JSON.parse(event.body))
     }else if(method === 'GET'){
         const info_controller = new InfoController(info_service)
         return await info_controller.getInfo()
